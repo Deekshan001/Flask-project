a7a7d3c41591a2440c6cbff3295c5eb01b798a1a
Deekshan <sdeekshan001@gmail.com>
2021-05-05 13:06:12 +0530

    stack app

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template,request
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -72,6 +72,64 @@ def stackData():
         data.append(n[9])
         return data
 
+class Conversion:
+	def __init__(self, capacity):
+		self.top = -1
+		self.capacity = capacity
+		self.array = []
+		self.output = []
+		self.precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
+
+	def isEmpty(self):
+		return True if self.top == -1 else False
+	def peek(self):
+		return self.array[-1]
+	def pop(self):
+		if not self.isEmpty():
+			self.top -= 1
+			return self.array.pop()
+		else:
+			return "$"
+	def push(self, op):
+		self.top += 1
+		self.array.append(op)
+	def isOperand(self, ch):
+		return ch.isalpha()
+	def notGreater(self, i):
+		try:
+			a = self.precedence[i]
+			b = self.precedence[self.peek()]
+			return True if a <= b else False
+		except KeyError:
+			return False
+			
+	def infixToPostfix(self, exp):
+		table=[]
+		for i in exp:
+			if self.isOperand(i):
+				self.output.append(i)
+			elif i == '(':
+				self.push(i)
+			elif i == ')':
+				while( (not self.isEmpty()) and
+								self.peek() != '('):
+					a = self.pop()
+					self.output.append(a)
+				if (not self.isEmpty() and self.peek() != '('):
+					return -1
+				else:
+					self.pop()
+			else:
+				while(not self.isEmpty() and self.notGreater(i)):
+					self.output.append(self.pop())
+				self.push(i)
+			table.append([i,", ".join(self.array),"".join(self.output)])
+		while not self.isEmpty():
+			self.output.append(self.pop())
+		table.append(["_","_","".join(self.output)])
+		table.append("".join(self.output))        
+		return table
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -105,6 +163,18 @@ def queueDes():
 @app.route("/stackDes")
 def stackDes():
     return render_template("stackDes.html",info=stackData())
+
+@app.route('/stackApp')
+def stackApp():
+    return render_template('stackApp.html',postfix=[""],ln=0)
+
+@app.route('/stackApp', methods=['POST'])
+def stackApp_post():
+    exp = request.form['text']
+    obj = Conversion(len(exp))
+    result=obj.infixToPostfix(exp)
+    return render_template('stackApp.html',infix=exp,postfix=result,ln=len(result))
+
 if __name__ == "__main__":
     app.run(debug=True)
 
